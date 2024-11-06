fb89ffc66a5b795b154f2b94dc73ba2ca015f2ac
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-11-06 12:36:37 -0300

    tsl

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -7,38 +7,282 @@ const users = [
   {
     name: "Pedro Perez",
     slug: "pedro-perez",
-    calendlySlug: "pedro-perez",
-    calendlyPage: "/calendly/pedro-perez",
-    calendlyLink: "https://calendly.com/nicoalonso99-na",
-    historyPart1: [
-      "Siempre me llamó la atención el mundo del <strong>emprendimiento</strong>, los <strong>negocios</strong> y la formación de <strong>empresas</strong>. Me recibí de <strong>Técnico en administración de empresas</strong>, estudié 1 año en la <strong>UBA</strong> y 2 años en la <strong>UADE</strong>. Trabajé un año en <strong>IBM</strong>, y eso fue más que suficiente para darme cuenta que el <strong>sistema tradicional</strong> no me iba a dar la <strong>vida</strong> que quería.",
-      "Ahí inició mi carrera como <strong>emprendedor</strong>, a los 21 años compré un <strong>auto</strong> a crédito para ponerlo a hacer <strong>UBER</strong>, avancé con un <strong>local de comida</strong>, después llegó a mi vida el mundo de los <strong>negocios digitales</strong>. Me presentaron la empresa <strong>AMWAY</strong>, que es una empresa <strong>Norteamericana</strong> que lleva más de 60 años en funcionamiento, donde vos podrías distribuir sus <strong>productos</strong>, comisionar por tus <strong>ventas</strong> y armar <strong>equipos de trabajo</strong>. Gracias a esa actividad conocí más de <strong>20 países</strong> y gané mucho <strong>dinero</strong> durante 5 años. Después armé una empresa de <strong>ventas de planes de autos</strong> con 2 amigos, puse una <strong>oficina</strong> con personas haciendo <strong>arbitraje</strong>, armé una <strong>comunidad de cripto players</strong> en <strong>AXIE INFINITY</strong>, pero no me fue tan bien como planeaba...",
-    ],
-
-    historyPart2: [
-      "Después me empezó a llamar la atención el <strong>Trading</strong> y fue ahí cuando conocí a <strong>TSL</strong> y <strong>Pedro Perez</strong>, y me encantó, en el minuto uno supe que acá sí podía hacer <strong>carrera</strong> y dedicar años de mi vida a convertirme en un <strong>profesional</strong> e impactar de forma positiva a más personas.",
-      "Me encanta ponerme <strong>metas</strong> y trabajar con <strong>personas</strong> así que cuando entendí lo potente del <strong>trading</strong> y el <strong>plan de mercadeo</strong> lo empecé a escalar, mis primeros 5 meses me costó un poco superar los <strong>usd 500 de ganancias mensuales</strong>, hoy en día soy un <strong>trader rentable</strong> y el principal <strong>creador de equipos</strong> de <strong>TSL</strong>, gano más de <strong>usd 30.000 al mes</strong>, vivo en una <strong>casa soñada</strong>, viajo seguido, ayudo económicamente a mis <strong>viejos</strong> para que no les falte nada, y lo mejor de todo es que hay cientos de personas en el <strong>equipo</strong> ganando <strong>cientos y miles de dólares extras</strong> al mes gracias a tomar la decisión de emprender con nosotros en <strong>TSL</strong>.",
-    ],
-
-    historyPart3: [
-      "Te invitamos a dar el <strong>paso</strong>, anímate a hacer algo <strong>distinto</strong>, tenemos un <strong>método</strong> que funciona, que ya fue testeado, y constantemente lo estamos <strong>mejorando</strong>.",
-      "Si vos das tu <strong>100%</strong> vas a poder <strong>progresar</strong> y hacer <strong>carrera</strong> con nosotros. Lo <strong>tradicional</strong> ya no funciona, el mundo cambió, las <strong>oportunidades</strong> están, hay personas que las aprovechan y otras no! ¡Animate! ¡Nosotros te ayudamos!",
-    ],
-    actually: [
-      "Facturé más de 200.000 USD con TSL, vivo en Argentina y tengo amigos que comparten los mismos objetivos.",
-      "El dinero ya no es un problema.",
-      "Puedo cuidar de mí mismo, de mi familia, de mis amigos y, lo más importante…",
-      "Puedo aportarles a personas que son como yo.",
-      "Y ahora seguramente te estarás preguntando, ¿cómo es que este joven argentino creció tanto y tan rápido con TSL?",
-      ` Acá los llamamos "los tres pilares" de este nuevo modelo de negocio…`,
-    ],
-    historyData: ["Pedro Perez", "Argentina", "5 años"],
-    historyImages: [cristian1, cristian2, cristian3, cristian4],
-    googleSheets:
-      "https://script.google.com/macros/s/AKfycbzZH7_GuBEMwlnWmeZ_0MuMSIdUgkD0miOCbuew8ps8OcTYYB491XItHXlnxXJb4AhOpg/exec",
     cwppLink:
       "https://wa.me/515857574?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
     wppNumber: "+549126997232",
+  },
+  {
+    name: "Fermín Martínez",
+    slug: "fermin-martinez",
+    cwppLink:
+      "https://wa.me/5492921425518?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5492921425518",
+  },
+  {
+    name: "Acosta Erika",
+    slug: "acosta-erika",
+    cwppLink:
+      "https://wa.me/5491140585192?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491140585192",
+  },
+  {
+    name: "Jonatan Alexis Bibe",
+    slug: "jonatan-alexis-bibe",
+    cwppLink:
+      "https://wa.me/5492922430455?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5492922430455",
+  },
+  {
+    name: "German Stagliano",
+    slug: "german-stagliano",
+    cwppLink:
+      "https://wa.me/5491166716496?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491166716496",
+  },
+  {
+    name: "Hernán Saucedo",
+    slug: "hernan-saucedo",
+    cwppLink:
+      "https://wa.me/5491157053886?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491157053886",
+  },
+  {
+    name: "Eber Perez",
+    slug: "eber-perez",
+    cwppLink:
+      "https://wa.me/5491127236374?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491127236374",
+  },
+  {
+    name: "Ivan Winchester",
+    slug: "ivan-winchester",
+    cwppLink:
+      "https://wa.me/5491153411096?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491153411096",
+  },
+  {
+    name: "Eduardo Ezequiel Guzmán",
+    slug: "eduardo-ezequiel-guzman",
+    cwppLink:
+      "https://wa.me/5491128313864?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491128313864",
+  },
+  {
+    name: "Leonardo Quintana",
+    slug: "leonardo-quintana",
+    cwppLink:
+      "https://wa.me/5492255410233?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5492255410233",
+  },
+  {
+    name: "Nicolas Balverdi",
+    slug: "nicolas-balverdi",
+    cwppLink:
+      "https://wa.me/5491126203864?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491126203864",
+  },
+  {
+    name: "Alexandra Nadir Vieyra",
+    slug: "alexandra-nadir-vieyra",
+    cwppLink:
+      "https://wa.me/5493572409972?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5493572409972",
+  },
+  {
+    name: "Matias Ferreira",
+    slug: "matias-ferreira",
+    cwppLink:
+      "https://wa.me/5491171553024?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491171553024",
+  },
+  {
+    name: "German Gimenez",
+    slug: "german-gimenez",
+    cwppLink:
+      "https://wa.me/5491164256457?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491164256457",
+  },
+  {
+    name: "Soledad Andrea Gomez",
+    slug: "soledad-andrea-gomez",
+    cwppLink:
+      "https://wa.me/5492255425950?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5492255425950",
+  },
+  {
+    name: "Brenda Fernandez",
+    slug: "brenda-fernandez",
+    cwppLink:
+      "https://wa.me/5491125132786?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491125132786",
+  },
+  {
+    name: "Raul Crisorio",
+    slug: "raul-crisorio",
+    cwppLink:
+      "https://wa.me/5493518170429?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5493518170429",
+  },
+  {
+    name: "Nicolas Ruibal",
+    slug: "nicolas-ruibal",
+    cwppLink:
+      "https://wa.me/5491154042633?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491154042633",
+  },
+  {
+    name: "Joaquin Dominguez",
+    slug: "joaquin-dominguez",
+    cwppLink:
+      "https://wa.me/5491173645792?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491173645792",
+  },
+  {
+    name: "Claudio Zalate",
+    slug: "claudio-zalate",
+    cwppLink:
+      "https://wa.me/5491158507878?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491158507878",
+  },
+  {
+    name: "Gabriel Cardinale",
+    slug: "gabriel-cardinale",
+    cwppLink:
+      "https://wa.me/5491170585404?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491170585404",
+  },
+  {
+    name: "Maximiliano Gustavo Moyano",
+    slug: "maximiliano-gustavo-moyano",
+    cwppLink:
+      "https://wa.me/5491153833901?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491153833901",
+  },
+  {
+    name: "Paloma Hope",
+    slug: "paloma-hope",
+    cwppLink:
+      "https://wa.me/5492213510520?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5492213510520",
+  },
+  {
+    name: "Marcela Medina",
+    slug: "marcela-medina",
+    cwppLink:
+      "https://wa.me/5493812189417?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5493812189417",
+  },
+  {
+    name: "Matías Fernández",
+    slug: "matias-fernandez",
+    cwppLink:
+      "https://wa.me/5493515069683?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5493515069683",
+  },
+  {
+    name: "Martin Rodrigo Merbilhaa",
+    slug: "martin-rodrigo-merbilhaa",
+    cwppLink:
+      "https://wa.me/542215404623?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+542215404623",
+  },
+  {
+    name: "Luis Guajardo Velasquez",
+    slug: "luis-guajardo-velasquez",
+    cwppLink:
+      "https://wa.me/56934314103?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+56934314103",
+  },
+  {
+    name: "Gonzalo Recasens",
+    slug: "gonzalo-recasens",
+    cwppLink:
+      "https://wa.me/5491181075414?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491181075414",
+  },
+  {
+    name: "Matias Moyano",
+    slug: "matias-moyano",
+    cwppLink:
+      "https://wa.me/5491161258912?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491161258912",
+  },
+  {
+    name: "Pablo Scorpo",
+    slug: "pablo-scorpo",
+    cwppLink:
+      "https://wa.me/5493518598485?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5493518598485",
+  },
+  {
+    name: "Walter Cortes",
+    slug: "walter-cortes",
+    cwppLink:
+      "https://wa.me/5491134458006?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491134458006",
+  },
+  {
+    name: "Natalia Rocio Juarez",
+    slug: "natalia-rocio-juarez",
+    cwppLink:
+      "https://wa.me/5492236955385?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5492236955385",
+  },
+  {
+    name: "María Jose Medina",
+    slug: "maria-jose-medina",
+    cwppLink:
+      "https://wa.me/5492615468225?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5492615468225",
+  },
+  {
+    name: "Leonardo Alvarez",
+    slug: "leonardo-alvarez",
+    cwppLink:
+      "https://wa.me/5491123718929?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491123718929",
+  },
+  {
+    name: "Mario Rojas",
+    slug: "mario-rojas",
+    cwppLink:
+      "https://wa.me/5491126539038?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491126539038",
+  },
+  {
+    name: "Javier Andres Cortez",
+    slug: "javier-andres-cortez",
+    cwppLink:
+      "https://wa.me/549468584158?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+549468584158",
+  },
+  {
+    name: "Candela Aymara Gutierrez",
+    slug: "candela-aymara-gutierrez",
+    cwppLink:
+      "https://wa.me/5491171279102?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491171279102",
+  },
+  {
+    name: "Marcos Gauna",
+    slug: "marcos-gauna",
+    cwppLink:
+      "https://wa.me/5491124092465?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491124092465",
+  },
+  {
+    name: "Mariana Soledad Gómez",
+    slug: "mariana-soledad-gomez",
+    cwppLink:
+      "https://wa.me/5491171194239?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491171194239",
+  },
+  {
+    name: "Camila Balsante",
+    slug: "camila-balsante",
+    cwppLink:
+      "https://wa.me/5491171073175?text=Hola%2C%20estoy%20interesado%20en%20tus%20servicios",
+    wppNumber: "+5491171073175",
   },
 ];
 
