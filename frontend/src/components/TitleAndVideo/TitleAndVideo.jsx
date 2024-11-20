35e7ba90c73f20669eef76cf29970100370f98db
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-11-20 19:12:47 -0300

    TSL

--- frontend/src/components/TitleAndVideo/TitleAndVideo.jsx
+++ frontend/src/components/TitleAndVideo/TitleAndVideo.jsx
@@ -4,16 +4,15 @@ const TitleAndVideo = ({ video, calendlyPage, data }) => {
   return (
     <div className="flex flex-wrap justify-center items-start text-center space-y-4">
       <h1 className="lato-black text-gray-200 text-xl lg:text-5xl text-balance uppercase">
-        Trader Argentino{" "}
-        <span className="text-[#289ff0]">Revoluciona la Industria</span> con un
-        Sistema Que{" "}
+        <span className="text-[#289ff0]"> Trader Argentino </span> Comparte un
+        Sistema Probado que
         <span className="text-[#289ff0]">
-          Cualquiera Puede Copiar y Aplicar
+          Cualquiera Puede Aprender y Aplicar
         </span>
       </h1>
 
       <p className="lato-regular-italic text-sm lg:text-lg text-gray-100">
-      Más de $2.500.000 generados en trading
+      Con más de 2000 miembros en nuestra comunidad, ofrecemos el sistema más efectivo y probado del mercado para ayudarte a ser rentable en trading.
       </p>
       <div className="flex justify-center w-full">
         {video === "wpp" && (
